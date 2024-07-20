ea6aa2a7c34166395a6107b6bc84f03d87efe180
Ajay <ajay.30904@gmail.com>
2024-07-20 13:19:35 -0700

    fixing camera panning.

--- Assets/Scripts/Gameplay/CamerControls.cs
+++ Assets/Scripts/Gameplay/CamerControls.cs
@@ -17,17 +17,30 @@ public class CamerControls : MonoBehaviour
     public float duration;
     public CharacterController2D playerController;
     public CinemachineVirtualCamera virtualCamera;
+    public CinemachineFramingTransposer framingTransposer;
     public bool oneShot;
     public bool isEnabled;
 
     private float initialOrthographicSize;
     public  float targetOrthographicSize;
     public  float elapsedTime;
+    public  float screenStartX;
+    public  float screenStartY;
+    public  float screenX;
+    public  float screenY;
+    private bool isFlipped;
+
+    public bool canLerpScreen;
+    public bool canLerpSize;
+
+
 
     private void Awake()
     {
         playerController = GameObject.Find("player").GetComponent<CharacterController2D>();
         virtualCamera = GameObject.Find("virtualCamera").GetComponent<CinemachineVirtualCamera>();
+        framingTransposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+       
 
         initialOrthographicSize = virtualCamera.m_Lens.OrthographicSize;
         if (controlType == ECameraControl.EPAN_IN)
@@ -38,16 +51,49 @@ public class CamerControls : MonoBehaviour
         elapsedTime = 0.0f;
     }
 
+    private void Start()
+    {
+        if (virtualCamera.GetComponent<CinemachinePixelPerfect>())
+        {
+            virtualCamera.GetComponent<CinemachinePixelPerfect>().enabled = false;
+        }
+    }
+
     private void Update()
     {
         if (isEnabled)
         {
             elapsedTime += Time.deltaTime;
             float t = Mathf.Clamp01(elapsedTime / duration);
-            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(initialOrthographicSize, targetOrthographicSize, t);
+            if(canLerpSize)
+            {
+                virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(initialOrthographicSize, targetOrthographicSize, t);
+                Debug.Log("OrthographicSize: " + virtualCamera.m_Lens.OrthographicSize);
+            }
 
+            if (canLerpSize)
+            {
+                if (isFlipped)
+                {
+                    t = targetOrthographicSize / virtualCamera.m_Lens.OrthographicSize;
+                }
+                else
+                {
+                    t = virtualCamera.m_Lens.OrthographicSize / targetOrthographicSize;
+                }
 
-            if(Mathf.Approximately(virtualCamera.m_Lens.OrthographicSize, targetOrthographicSize))
+            }
+
+            if (canLerpScreen)
+            {
+                framingTransposer.m_ScreenX  = Mathf.Lerp(screenStartX, screenX, t);
+                framingTransposer.m_ScreenY  = Mathf.Lerp(screenStartY, screenY, t);
+
+                Debug.Log("m_ScreenX: "+framingTransposer.m_ScreenX);
+                Debug.Log("m_ScreenY: "+framingTransposer.m_ScreenY);
+            }
+
+            if (Mathf.Approximately(virtualCamera.m_Lens.OrthographicSize, targetOrthographicSize))
             {
                 isEnabled = false;
                 virtualCamera.m_Lens.OrthographicSize = targetOrthographicSize;
@@ -60,6 +106,9 @@ public class CamerControls : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             initialOrthographicSize = virtualCamera.m_Lens.OrthographicSize;
+            isFlipped = initialOrthographicSize > targetOrthographicSize;
+            screenStartX = framingTransposer.m_ScreenX;
+            screenStartY = framingTransposer.m_ScreenY;
             isEnabled = true;
             elapsedTime = 0.0f;
         }
